7e3a1e35c01e74f937c9dd1bc1492a3e09b96299
Bipeen swaraj <120006987+bipeenswaraj@users.noreply.github.com>
2024-07-05 17:42:33 +0530

    Add files via upload

---
+++ binarysearch.cpp
@@ -0,0 +1,82 @@
+
+#include <iostream>
+#include <array>
+using namespace std;
+
+
+int binarySearch(int arr[],int key,int size){
+    int low = 0;
+    int high = size - 1;
+    
+    while(low<=high){
+        int mid =  (high + low) / 2;
+        if(key==arr[mid]){
+            return mid;
+        }
+        
+        if(key>arr[mid]){
+            low = mid+1;
+        }
+        else{ 
+            high = mid - 1;
+        }
+    }
+    return -1;
+}
+void inputarray(int*arr,int size){
+     for(int i = 0;i<size;i++){
+       cin>>arr[i];
+     }
+}
+void swap(int &a,int &b){
+    int temp = a;
+    a = b;
+    b = temp;
+}
+void sortting(int arr[],int size){
+    for(int i = 0;i<size - 1;i++){
+        for(int j = 0;j<size - 1 -i;j++){
+            if(arr[j]>arr[j+1]){
+                swap(arr[j],arr[j+1]);
+            }
+        }
+    }
+}
+void printArray(int arr[],int size){
+    for(int i=0; i<size;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
+
+int main()
+{
+   int size;
+   cout<<"enter size of array"<<endl;
+   cin>>size;
+   int* arr = new int[size];
+   int key;
+   cout<<"Enter array elements:"<<endl;
+   inputarray(arr,size);
+   cout<<"original array:"<<endl;
+   printArray(arr,size);
+   cout<<"array is sorted."<<endl;
+   sortting(arr,size);
+   printArray(arr,size);
+   cout<<"Enter element to search:"<<endl;
+   cin>>key;
+   int result = binarySearch(arr,key,size);
+
+   if(result == -1){
+       cout<<"Not Found";
+   }
+   else{
+       cout<<key<<" Found at index: "<<result;
+   }
+  
+    
+    return 0;
+}
+// create an unsorted array from user input of size at least 6.
+// sort it using bubblesort
+// find an element from user input using binarySearch.
